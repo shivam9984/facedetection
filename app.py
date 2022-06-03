f4d27c832d40ceb44f4dcb8da2a15812260a4fba
Ubuntu <kushal@Android-Malicious-App.2iesobiyctmu3jef5aahlfuukb.rx.internal.cloudapp.net>
2022-06-03 14:09:44 +0000

    ...

--- app.py
+++ app.py
@@ -77,4 +77,6 @@
 
 from streamlit_webrtc import webrtc_streamer
 
-webrtc_streamer(key="sample")
+webrtc_streamer(key="sample", rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]
+    })
