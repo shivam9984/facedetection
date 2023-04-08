5469d347b036ff4d543baad7a31ce871f69f4d3c
shivam9984 <74639089+shivam9984@users.noreply.github.com>
2023-04-08 12:46:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import streamlit as strtc
+import streamlit as st
 from tensorflow import keras
 from keras.models import model_from_json
 from tensorflow.keras.utils import img_to_array
